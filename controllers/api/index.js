71303afa4f3188c5fccb4b3fb30a1d77e3f32efd
Casey Mcgrath <caseymcgrath1000@gmail.com>
2024-01-26 15:25:41 -0500

    Added project api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const projectRoutes = require('./projectRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/projects', projectRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const {Project} = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const projectData = await Project.findAll();
+
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newProject = await Project.create({
+            ...req.body,
+        });
+        res.status(200).json(newProject)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const projectData = await Project.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+
+        if(!projectData) {
+            res.status(400).json({message: `No project found with this ID by this user`})
+        }
+
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+
+module.exports = router;
